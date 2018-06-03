20c48aba172dcdb30107a22bf26bfcb84ca67daf
Genric Jan Elazegui <gj.elazegui@Gmail.com>
2018-06-02 20:35:39 -0400

    Finished Title Menu

---
+++ healthBar/Assets/Scenes/Scripts/menuButtons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuButtons : MonoBehaviour
+{
+
+    public Animator anim;
+
+    public void PlayGame()
+    {
+        anim.SetTrigger("FadeOut");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+
+    public void onFadeComplete()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
